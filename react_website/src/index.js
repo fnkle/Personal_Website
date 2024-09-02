50c44a76db48c11f77cd125ee39f5d55976d0d4f
Finkle69 <100445767+Finkle69@users.noreply.github.com>
2024-09-02 18:43:01 +0100

    React finished

--- react_website/src/index.js
+++ react_website/src/index.js
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
-import { Component, useState } from 'react';
+
+import { Component } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css"
@@ -19,8 +19,8 @@ class Banner extends Component {
 }
 
 const SubTitle = (props) => {
-    return <div class="subTitle" id={this.props.titleId}>
-        <h2>{this.props.text}</h2>
+    return <div className="subTitle" id={props.titleId}>
+        <h2>{props.titleText}</h2>
     </div>
 }
 
@@ -29,8 +29,8 @@ class UniversityProjects extends Component {
         const Projects = [
             {
                 id: "uniAssignment 1",
-                title: 'Programming Using an Object-Orientated Language - Revision App',
-                decription: "For this module our main assignment was to code a revision app for use in a school; this was my first introduction to the Java programming language. I made use of various OOP principals such as inheritance, abstract classes and interfaces in an attempt to create a highly cohesive application with low coupling. I am unable to share the code due to university policy",
+                title: "Programming Using an Object-Orientated Language - Revision App",
+                description: "For this module our main assignment was to code a revision app for use in a school; this was my first introduction to the Java programming language. I made use of various OOP principals such as inheritance, abstract classes and interfaces in an attempt to create a highly cohesive application with low coupling. I am unable to share the code due to university policy",
                 moduleMark: "84%",
                 assignmentMark: "82%",
                 date: "Year 1 Semester 2",
@@ -46,40 +46,94 @@ class UniversityProjects extends Component {
         ]
 
 
-        let projectsHTML = Projects.map((project) => {
+        const projectsHTML = Projects.map((project) => {
             return (
-                <div className="project" id={project.id}>
-                <h2>{project.title}</h2>
-                <h3>Description</h3>
-                <p>
-                    {project.description}
-                </p>
-                <h3>Marks and Feedback</h3>
-                <ul>
-                    <li>Overall Module Mark: {project.moduleMark}</li>
-                    <li>Programming Assignment Mark: {project.assignmentMark}</li>
-                </ul>
-                <h3>Date</h3>
-                <p>{project.date}</p>
-            </div>
+                <div key={crypto.randomUUID()} className="project" id={project.id}>
+                    <h2 key={crypto.randomUUID()}>{project.title}</h2>
+                    <h3 key={crypto.randomUUID()}>Description</h3>
+                    <p key={crypto.randomUUID()}>
+                        {project.description}
+                    </p>
+                    <h3 key={crypto.randomUUID()}>Marks</h3>
+                    <ul key={crypto.randomUUID()}>
+                        <li key={crypto.randomUUID()}>Overall Module Mark: {project.moduleMark}</li>
+                        <li key={crypto.randomUUID()}>Programming Assignment Mark: {project.assignmentMark}</li>
+                    </ul>
+                    <h3 key={crypto.randomUUID()}>Date</h3>
+                    <p key={crypto.randomUUID()}>{project.date}</p>
+                </div>
             )
         })
 
         return (
-           <>{projectsHTML}</>
+            <>{projectsHTML}</>
         )
     }
 }
 
 
+class PersonalProjects extends Component {
+    render() {
 
+        const Projects = [
+            {
+                title: "Snake",
+                description: "I have developed a basic snake game using C++, it is not complete but was used in order for me to start working with C++ and CMake in conjunction with SFML",
+                features: ["Wall collision", "Ability for user to modify values", "Tidy up code by making use of more functions"],
+                image: "snake.png",
+                projectid: "personalProject1",
+                imgid:"image1",
+                imgalt: "A screenshot of my snake game",
+            },
+            {
+                title: "Personal Website",
+                description: "An ongoing website of mine is this website in order to have a collection of my various personal projects. This will be updated as more projcts are created.",
+                features: ["About me section", "Update colour scheme", "Convert to React.js"],
+                image: "website.png",
+                projectid: "personalProject2",
+                imgid:"image2",
+                imgalt: "A screenshot of an old version of my site",
+            }
+        ]
+
+
+
+        const projectsHTML = Projects.map((project) => {
+            return (
+                <>
+                    <div className="project" id="personalProject1">
+                        <h2>{project.title}</h2>
+                        <h3>Description</h3>
+                        <p>{project.description}</p>
+                        <h3>Possible features</h3>
+                        <ul>
+                            {project.features.map((feature) => {
+                                return <li>{feature}</li>
+                            })}
+                        </ul>
+
+                    </div>
+                    <div className="projectImage" id={project.imgid}>
+                        <img src={project.image} alt={project.imgalt} />
+                    </div>
+                </>
+            )
+        })
+
+        return (
+            <>{projectsHTML}</>
+        )
+    }
+}
 
 
 root.render(
     <>
-    <Banner />
-    <SubTitle titleId="subTitle1" text="University Projects" />
-    <UniversityProjects />
+        <Banner />
+        <SubTitle titleId="subTitle1" titleText="University Projects" />
+        <UniversityProjects />
+        <SubTitle titleId="subTitle2" titleText="Personal Projects" />
+        <PersonalProjects />
     </>
 );
 
